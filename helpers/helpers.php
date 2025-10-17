cb51779805d2647df8f35d0ea606fefa438ed85b
nomoreerrors <nomoreerrors@gmail.com>
2025-10-17 09:33:12 +0300

    Changed api_key define method due to problems on beget hosting:
    -    $api_key = $_SERVER['HTTP_API_KEY'] ?? '';
    +    $api_key = $_SERVER['API_KEY'] ?? '';
    
         if ($api_key !== $_ENV['API_KEY']) {
    -    http_response_code(403);
    -    echo json_encode(['error' => 'Invalid API key']);
    -    exit;
    +        http_response_code(403);
    +        echo json_encode(['error' => 'Invalid API key', 'api_key' => $api_key]);
    +        exit;

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
-function cleanText($text): string {
+function cleanText($text): string
+{
     // Заменим специальные пробелы (неразрывный, тонкий и пр.) обычным пробелом
     $text = preg_replace('/[\x{00A0}\x{2009}\x{202F}\x{FEFF}]/u', ' ', $text);
 
@@ -15,12 +16,12 @@ function checkAccess(): void
     // Нужна здесь на случай echo case
     header('Content-Type: application/json');
 
-    $api_key = $_SERVER['HTTP_API_KEY'] ?? '';
+    $api_key = $_SERVER['API_KEY'] ?? '';
 
     if ($api_key !== $_ENV['API_KEY']) {
-    http_response_code(403);
-    echo json_encode(['error' => 'Invalid API key']);
-    exit;
+        http_response_code(403);
+        echo json_encode(['error' => 'Invalid API key', 'api_key' => $api_key]);
+        exit;
     }
     return;
 }
